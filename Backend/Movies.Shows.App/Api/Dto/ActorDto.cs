a54ea269f01a521a363bcb4845b5530331b3c7c9
kobedeschuytter1 <kobe.deschuytter@hotmail.com>
2022-10-21 10:34:54 +0200

    creating controllers/dtos

---
+++ Backend/Movies.Shows.App/Api/Dto/ActorDto.cs
@@ -0,0 +1,25 @@
+﻿namespace Api.Dto
+{
+    public class ActorDto
+    {
+        public class Index
+        {
+
+        }
+        public class Detail
+        {
+
+        }
+
+        public class Create
+        {
+
+        }
+
+        public class Delete
+        {
+
+
+        }
+    }
+}
